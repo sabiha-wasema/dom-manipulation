42f713a7a00ac1c61478b6eb36d6c271801145c2
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-11-24 17:10:52 +0600

    counter section added

--- js/counter.js
+++ js/counter.js
@@ -1,32 +1,46 @@
-const counterContainer = document.getElementById('counter-container');
-const experienceContainer = document.createElement('div');
-const experienceHeader = document.createElement('h1');
+const counterContainer = document.getElementById("counter-container");
+const experienceContainer = document.createElement("div");
+const experienceHeader = document.createElement("h1");
 const counterArray = [
-    { title: "15+", description: "Year Of Experience" },
-    { title: "20+", description: "Completed Projects" },
-    { title: "10+", description: "Awards Won" },
-    { title: "5+", description: "Global Partners" },
-  ];
+  { title: "500", description: "Year Of Experience" },
+  { title: "700", description: "Completed Projects" },
+  { title: "100", description: "Awards Won" },
+  { title: "400", description: "Global Partners" },
+];
 
-  const singleExperienceContainer = document.createElement('div');
-  singleExperienceContainer.classList.add('single-experience-item');
-counterArray.forEach(item =>{
-    const experienceItem = document.createElement('div');
-    experienceItem.classList.add('single-item')
-    experienceItem.innerHTML = `
-    <p class="experience-year">${item.title}</p>
+const singleExperienceContainer = document.createElement("div");
+singleExperienceContainer.classList.add("single-experience-item");
+
+const incrementCounter = (element, target) => {
+  let count = 0;
+  const interval = setInterval(() => {
+    if (count < target) {
+      count++;
+      element.textContent = `${count}+`;
+    } else {
+      clearInterval(interval);
+    }
+  }, 50);
+};
+
+counterArray.forEach((item) => {
+  const experienceItem = document.createElement("div");
+  experienceItem.classList.add("single-item");
+  experienceItem.innerHTML = `
+    <p class="experience-year">0+</p>
     <p class="experience-heading">${item.description}</p>
- 
 `;
-singleExperienceContainer.appendChild(experienceItem)
-})
-counterContainer.appendChild(singleExperienceContainer)
+  const counterElement = experienceItem.querySelector(".experience-year");
+  incrementCounter(counterElement, item.title);
 
+  singleExperienceContainer.appendChild(experienceItem);
+});
+counterContainer.appendChild(singleExperienceContainer);
 
 // experienceHeader.innerHTML = `
 // <p class="experience-year">${title}</p>
 // <p class="experienceHeading">${description}</p>
 // `;
 experienceContainer.appendChild(experienceHeader);
-console.log(experienceContainer)
+console.log(experienceContainer);
 counterContainer.appendChild(experienceContainer);
